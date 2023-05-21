afd7019a53da3826f0af1c7c0a7ab79149557660
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2023-05-20 22:53:45 -0300

    Cálculo do IMC

---
+++ Aula12.py
@@ -0,0 +1,12 @@
+nome = 'Raul Maximiliano'
+altura = 1.90
+peso = 83
+imc = peso / altura ** 2
+
+print(nome, 'tem', altura, 'de altura,',)
+print('pesa', peso, 'quilos e seu imc é',)
+print(imc)
+
+# Raul Maximiliano tem 1.90 de altura,
+# pesa 83 quilos e seu IMC é
+# 22.991689750692522
