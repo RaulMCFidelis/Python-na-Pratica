27d8af6ad4356c2166d0da618331a5f67e7dbc20
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2023-09-23 01:39:44 -0300

    try e except

--- Full-Stack-Python/Aula29.py
+++ Full-Stack-Python/Aula29.py
@@ -7,14 +7,14 @@ numero_str = input(
     'Vou dobrar o número que vc digitar: '
 )
 
-try:
+try: # Tenta executar o código a seguir até dar um erro e se der erro, excuta o except
     numero_float = float(numero_str)
     print('FLOAT:', numero_float)
     print(f'O dobro de {numero_str} é {numero_float * 2:.2f}')
 except:
     print('Isso não é um número')
 
-# if numero_str.isdigit():
+# if numero_str.isdigit(): Checando uma condição e muda o curso / não evita erros/exceções
 #     numero_float = float(numero_str)
 #     print(f'O dobro de {numero_str} é {numero_float * 2:.2f}')
 # else:
