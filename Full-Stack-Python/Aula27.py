a6dfeab2df1cf81cce54f2eb35b083ade3fd8416
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2023-09-23 00:49:12 -0300

    fatiamento de strings e a função len

--- Full-Stack-Python/Aula27.py
+++ Full-Stack-Python/Aula27.py
@@ -8,4 +8,8 @@ Obs.: a função len retorna a qtd
 de caracteres da str
 """
 variavel = 'Olá mundo'
-print(variavel[::-1])
+print(variavel[::-1]) #Escrevendo a string de trás para frente
+print(variavel[0:9:2]) # Pulando as letras de 2 em 2 no caso
+print(len(variavel))
+#len conta os caracteres, no caso 9
+#Espaço conta como caractere 
