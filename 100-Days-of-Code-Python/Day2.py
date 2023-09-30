519d769b8a66838eff913d644fab314eedcbb130
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2023-09-30 17:46:52 -0300

    Tip Calculator

--- 100-Days-of-Code-Python/Day2.py
+++ 100-Days-of-Code-Python/Day2.py
@@ -3,8 +3,8 @@
 #Round the result to 2 decimal places.
 print("Welcome to the tip calculator!")
 bill = float(input("What was the total bill? $"))
-tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
-people = int(input("How many people to split the bill?"))
+tip = int(input("How much tip would you like to give? 10, 12, or 15?\n"))
+people = int(input("How many people to split the bill?\n"))
 
 tip_as_percent = tip / 100
 total_tip_amount = bill * tip_as_percent
